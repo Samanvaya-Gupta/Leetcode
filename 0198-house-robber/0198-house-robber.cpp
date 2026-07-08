0d3a221310956b1ebd6c51e516dec2ef530bb5b6
Samanvaya Gupta <samanvaya.15.gupta@gmail.com>
2026-07-08 16:18:57 +0530

    Time: 0 ms (100%), Space: 10.7 MB (23.18%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++){
+            int pick = nums[i];
+            if(i>1) pick+=dp[i-2];
+            int nonpick = dp[i-1];
+            dp[i] = max(pick, nonpick);
+        }
+        return dp[n-1];
+    }
+};
