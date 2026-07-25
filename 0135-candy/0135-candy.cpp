e76bcd38d33483be38b4071e72adae18915f4e9d
Samanvaya Gupta <samanvaya.15.gupta@gmail.com>
2026-07-26 03:17:14 +0530

    Time: 4 ms (14.66%), Space: 22.2 MB (76.42%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,24 +2,28 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> left(n, -1);
-        left[0] = 1;
-        vector<int> right(n, -1);
-        right[n-1] = 1;
-        for(int i=1; i<n; i++){
-            if(ratings[i]>ratings[i-1]) left[i] = left[i-1]+1;
-            else left[i] = 1;
+        int i=1;
+        int sum = 1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum++;
+                i++;
+                continue;
+            }
+            int peak=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                sum+=peak;
+                i++;
+            }
+            int down=1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                down++;
+                i++;
+            }
+            if(down>peak) sum+= down-peak;
         }
-        for(int i=n-2; i>=0; i--){
-            if(ratings[i]>ratings[i+1]) right[i] = right[i+1]+1;
-            else right[i] = 1;
-        }
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            left[i] = max(left[i], right[i]);
-            ans += left[i];
-        }
-        return ans;
-        
+        return sum;
     }
 };
