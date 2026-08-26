821747d8f2947197f98fc9b14b8df63b3ca93e41
Samanvaya Gupta <samanvaya.15.gupta@gmail.com>
2026-08-26 16:34:44 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.96%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+private:
+    int solve(int n, vector<int>& dp){
+        if(n==1 || n==0) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+    }
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n, dp);
+    }
+};
