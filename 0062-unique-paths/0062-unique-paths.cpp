937d64abc685ddef6a9f50414aa7e97975cac5e0
Samanvaya Gupta <samanvaya.15.gupta@gmail.com>
2026-09-04 14:49:02 +0530

    Time: 3 ms (9.5%), Space: 9.5 MB (5.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -11,21 +11,20 @@ private:
     }
 
     int solveOpt(int m, int n){
-        vector<vector<int>> dp(m,vector<int> (n, 0));
-        dp[0][0] = 1;
+        vector<int> curr(n, 1);
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(i==0&&j==0) continue;
                 int up = 0;
                 int left = 0;
                 if(i>=1)
-                    up = dp[i-1][j];
+                    up = curr[j];
                 if(j>=1)
-                    left = dp[i][j-1];
-                dp[i][j] = up+left;
+                    left = curr[j-1];
+                curr[j] = up+left;
             }
         }
-        return dp[m-1][n-1];
+        return curr[n-1];
     }
 public:
     int uniquePaths(int m, int n) {
