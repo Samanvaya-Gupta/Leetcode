71365505e71ae4567c9582e10cee76f9af379a60
Samanvaya Gupta <samanvaya.15.gupta@gmail.com>
2026-09-04 14:44:22 +0530

    Time: 3 ms (9.5%), Space: 9.5 MB (5.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,35 @@
 class Solution {
 private:
-    int solve(int m, int n, vector<vector<int>>& dp){
-        if(m==0 && n==0) return 1;
-        if(m<0 || n<0) return 0;
+    int solve(int i, int j, vector<vector<int>>& dp){
+        if(i==0 && j==00) return 1;
+        if(i<0 || j<0) return 0;
 
-        if(dp[m][n]!=-1) return dp[m][n];
-        int up = solve(m-1, n, dp);
-        int left = solve(m, n-1, dp);
+        if(dp[i][j]!=-1) return dp[i][j];
+        int left = solve(i, j-1, dp);
+        int up = solve(i-1, j, dp);
+        return dp[i][j] = left+up;
+    }
 
-        return dp[m][n]=up+left;
+    int solveOpt(int m, int n){
+        vector<vector<int>> dp(m,vector<int> (n, 0));
+        dp[0][0] = 1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0&&j==0) continue;
+                int up = 0;
+                int left = 0;
+                if(i>=1)
+                    up = dp[i-1][j];
+                if(j>=1)
+                    left = dp[i][j-1];
+                dp[i][j] = up+left;
+            }
+        }
+        return dp[m-1][n-1];
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(m-1, n-1, dp);
+        vector<vector<int>> dp(m,vector<int> (n, -1));
+        return solveOpt(m, n);
     }
 };
