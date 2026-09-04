5e7c09fe0f6d3c79055ab9bcfefda3133198ff95
Samanvaya Gupta <samanvaya.15.gupta@gmail.com>
2026-09-04 15:08:02 +0530

    Time: 0 ms (100%), Space: 11.9 MB (31.39%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,21 +1,43 @@
 class Solution {
 private:
-    int solve(int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
-        if(m<0 || n<0) return 0;
-        if(obstacleGrid[m][n]==1) return 0;
-        if(m==0 && n==0) return 1;
+    int solve(vector<vector<int>>& obstacleGrid, int i, int j, vector<vector<int>>& dp){
+        if(i==0 && j==0){
+            if(obstacleGrid[i][j]==0) return 1;
+            else return 0;
+        }
+        if(i<0 || j<0) return 0;
+        if(obstacleGrid[i][j]==1) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up = solve(obstacleGrid, i-1, j, dp);
+        int left = solve(obstacleGrid, i, j-1, dp);
+        return dp[i][j] = up+left;
+    }
 
-        if(dp[m][n]!=-1) return dp[m][n];
-        int up = solve(m-1, n, obstacleGrid, dp);
-        int left = solve(m, n-1, obstacleGrid, dp);
-
-        return dp[m][n]=up+left;
+    int solveOpt(vector<vector<int>>& obstacleGrid, int m, int n){
+        vector<int> curr(n, 0);
+        if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1) return 0;
+        curr[0] = 1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0&&j==0) continue;
+                if(obstacleGrid[i][j]==1){
+                    curr[j]=0;
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+                if(i>0) up = curr[j];
+                if(j>0) left = curr[j-1];
+                curr[j] = up+left;
+            }
+        }
+        return curr[n-1];
     }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(m-1, n-1, obstacleGrid, dp);
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return solveOpt(obstacleGrid, m, n);
     }
 };
